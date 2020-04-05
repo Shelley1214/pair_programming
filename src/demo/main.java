2327aced052f1babebb13ae4fb4139e2b8aca1d9
murphyJUAN <elin880508@gmail.com>
2020-04-05 19:02:28 +0800

    murphy add student class in main.java

--- src/demo/main.java
+++ src/demo/main.java
@@ -7,49 +7,55 @@ import java.io.*;
 public class main {
 	
 	public static void main(String args[]) throws IOException{
-		Vector<Vector<Double>> data=new Vector<Vector<Double>>();
-		Vector<String> name = new Vector<String>();
 		
 		main A = new main();
-		A.InputData(data, name);
+		Vector<Student> studentList;
+		A.InputData(studentList);
 		
 		System.out.print("輸入ID或 Q (結束使用)?:\n");
 
-		int position = A.Input(A, data, name);
-		while (position == -1) position = A.Input(A, data, name); // Not exist
-		if (position == -2) return; //Q
+		Scanner scanner = new Scanner(System.in); 		
+		int position = A.Input(data, name, scanner); 		
+		while (position == -1) { 			
+			System.out.print("輸入ID或 Q (結束使用)?:\n"); 			
+			position = A.Input(data, name,scanner); 
+			// Not exist 		} 		
+			if (position == -2) return; //Q
+		}
 		
 		String Continue = "YES";
 		while(Continue.contentEquals("YES")) {
-			Scanner scanner = new Scanner(System.in);
 			System.out.print("輸入指令\n1) G顯示成績 \n2) R顯示排名 \n3) A顯示平均 \n4) W更新配分 \n5) E離開選單\n");
 			String num1 = scanner.next();
 			
 			if(num1.contentEquals("G")) {
-			
+				System.out.println(name.get(position)+ "成績如下：" + "\n");
+				System.out.println("Lab1 : "+ data.get(position).get(1) +  "\n");
+				System.out.println("Lab2 : "+ data.get(position).get(2) +  "\n");
+				System.out.println("Lab3 : "+ data.get(position).get(3) +  "\n");
+				System.out.println("Midterm : "+ data.get(position).get(4) +  "\n");
+				System.out.println("Final Exam : "+ data.get(position).get(5) +  "\n");
 			}else if(num1.contentEquals("R")) {
-			
+				
 			}else if(num1.contentEquals("A")) {
 				System.out.println(name.get(position)+ "平均成績為" + data.get(position).get(6));
 			}else if(num1.contentEquals("W")) {
 			
 			}else if(num1.contentEquals("E")) {
-				System.out.print("輸入​ID​或​ Q (​結束使用​)?");
-				position = A.Input(A, data, name);
-				while (position == -1) position = A.Input(A, data, name); // Not exist
-				if (position == -2) {
-					scanner.close();
+				System.out.print("輸入​ID​或​ Q (​結束使用​)?"); 				
+				position = A.Input(data, name,scanner); 				
+				while (position == -1) 
+					position = A.Input(data, name,scanner); // Not exist 				
+				if (position == -2) { 					
 					return; // Q
 				}
 			}else {
 				System.out.print("Wrong input!\n");
 			}
-			scanner.close();
 		}
 	}
 	
-	public int Input(main A, Vector<Vector<Double>>data, Vector<String>name) {
-		Scanner scanner = new Scanner(System.in);
+	public int Input(Vector<Vector<Double>>data, Vector<String>name, Scanner scanner) {
 		// Q or input ID
 		String num1 = scanner.next();
 		double num;
@@ -60,35 +66,33 @@ public class main {
 			num = Double.parseDouble(num1);
 		}
 		
-		int position = A.checkID(num,data);
+		int position = checkID(num,data);
 		if(position != -1) {
 			System.out.printf("Welcome, %s\n", name.get(position));
 		}
 		else {
 			System.out.print("無此人員!\n");
 		}
-		scanner.close();
 		return position;
 	}
 	
-	public void InputData(Vector<Vector<Double>> v1, Vector<String> name) throws IOException {
-
+	public void InputData(Vector<Student> studentList) throws IOException {
 		String cwd = System.getProperty("user.dir");
 		FileInputStream fr = new FileInputStream(cwd+"/input.txt");
 		Scanner inf = new Scanner(new InputStreamReader(fr,"UTF-8"));
-		
+		double weighted[] = {0.1,0.1,0.1,0.3,0.4};
 		while (inf.hasNext()) {
-			Vector<Double> v2 = new Vector<Double>();
-			v2.add(inf.nextDouble());
-			name.add(inf.next());
-			for (int i=0; i<5; i++) {
-				v2.add(inf.nextDouble());
-			}
-			double weighted = Weighted(v2, 0.1, 0.1, 0.1, 0.3, 0.4);
-			v2.add(weighted);
-			v1.add(v2);
+			int id = inf.nextInt();
+			String name = inf.next();
+			int lab1 = inf.nextInt();
+			int lab2 = inf.nextInt();
+			int lab3 = inf.nextInt();
+			int mid = inf.nextInt();
+			int final_exam = inf.nextInt();
+			Student newPerson = new Student(id, name, lab1, lab2, lab3, mid, final_exam);
+			newPerson.calValue(weighted);
+			studentList.add(newPerson);
 		}
-	
 		fr.close();
 	}
 	
@@ -121,4 +125,37 @@ public class main {
 	public void testCalculateTotalGrade() {
 		
 	}
+}
+
+class Student {
+	public int ID;
+	public String name;
+	public int lab1, lab2, lab3, mid, final_exam;
+	public double value;
+	
+	public Student(int ID_, String name_, int lab1_, int lab2_, int lab3_, int mid_, int final_exam_) {
+		ID = ID_;
+		name = name_;
+		lab1 = lab1_;
+		lab2 = lab2_;
+		lab3 = lab3_;
+		mid = mid_;
+		final_exam = final_exam_;
+	}
+	public void showGrade() {
+		System.out.println(name + "成績如下：" + "\n");
+		System.out.println("Lab1 : "+ lab1 +  "\n");
+		System.out.println("Lab2 : "+ lab2+  "\n");
+		System.out.println("Lab3 : "+ lab3 +  "\n");
+		System.out.println("Midterm : "+ mid +  "\n");
+		System.out.println("Final Exam : "+ final_exam +  "\n");
+	}
+	public void calValue(double weight[]) {
+		value = lab1*weight[0] + lab2*weight[1] + lab3*weight[2] +
+				mid*weight[3] + final_exam*weight[4];
+	}
+	public void showValue() {
+		System.out.println(name + "的平均成績為：" + value + "分" + "\n");
+	}
+	
 }
