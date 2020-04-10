3987ee4398a06241417f51abaaf25defd2c483bb
murphyJUAN <elin880508@gmail.com>
2020-04-11 01:43:52 +0800

    fix miss data caused by merge

--- src/demo/StudentTests.java
+++ src/demo/StudentTests.java
@@ -3,7 +3,6 @@ package demo;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Vector;
-import java.util.Scanner;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,7 +20,6 @@ class StudentTests {
 	@BeforeEach
 	void setUp() throws Exception {
 		GradeSystem TestGradeSys = new GradeSystem();
-//		Vector <Student> studentList = new Vector<Student>();
 		TestGradeSys.InputData(studentList, weighted, filename);
 		weighted[0] = 0.1;
 		weighted[1] = 0.1;
@@ -36,44 +34,65 @@ class StudentTests {
 		studentList.clear();
 		System.out.println("========== After test ============");
 	}
-
+	/**
+	 * test function: GradeSystem.checkID()
+	 * test data: 955002056
+	 */
 	@Test
 	public void checkIdTestPass() {
 		GradeSystem TestGradeSys = new GradeSystem();
 		assertEquals(1, TestGradeSys.checkID(955002056, studentList));
 		System.out.println("checkID_1");
 	}
-	
+	/**
+	 * test function: GradeSystem.checkID()
+	 * test data: 0
+	 */
 	@Test
 	public void checkIdTestFail() {
 		GradeSystem TestGradeSys = new GradeSystem();
 		assertEquals(-1, TestGradeSys.checkID(0, studentList));
 		System.out.println("checkID_2");
 	}
-	
-//	@Test
-//	public void changeWeightTestPass() throws Exception {
-//		System.out.println("changeWeight_1");
-//		GradeSystem TestGradeSys = new GradeSystem();
-//		double test_weighted[] = {0.2,0.2,0.2,0.2,0.2};
-//		String testSentence = "​lab1 20 lab2 20 lab3 20 mid-term 20 final exam 20";
-//		if(!weighted.equals(test_weighted)) throw new Exception("result wrong!");
-//	}
-	
+	/**
+	 * test function: GradeSystem.showRank()
+	 * test data: 
+	 * ID:962001044.0
+	 * studentList: 
+	 * {[ID:962001051, name:李威廷, lab1:81, lab2:32, lab3:50, mid-term:90, final_exam: 93],
+	 * 	[ID:955002056, name:許文馨, lab1:88, lab2:92, lab3:88, mid-term:98, final_exam: 91],
+	 * 	[ID:962001044, name:凌宗廷, lab1:87, lab2:86, lab3:98, mid-term:88, final_exam: 87]}
+	 */
 	@Test
 	public void showRankTestPass() {
 		GradeSystem TestGradeSys = new GradeSystem();
 		assertEquals(1, TestGradeSys.showRank(studentList, 962001044.0));
 		System.out.println("showRank_1");
 	}
-	
+	/**
+	 * test function: GradeSystem.showRank()
+	 * test data: 
+	 * ID:962001051.0
+	 * studentList: 
+	 * {[ID:962001051, name:李威廷, lab1:81, lab2:32, lab3:50, mid-term:90, final_exam: 93],
+	 * 	[ID:955002056, name:許文馨, lab1:88, lab2:92, lab3:88, mid-term:98, final_exam: 91],
+	 * 	[ID:962001044, name:凌宗廷, lab1:87, lab2:86, lab3:98, mid-term:88, final_exam: 87]}
+	 */
 	@Test
 	public void showRankTestFail() {
 		GradeSystem TestGradeSys = new GradeSystem();
 		assertEquals(2, TestGradeSys.showRank(studentList, 962001051.0));
 		System.out.println("showRank_2");
 	}
-	
+	/**
+	 * test function: GradeSystem.showGrade()
+	 * test data: 
+	 * ID:955002056
+	 * studentList: 
+	 * {[ID:962001051, name:李威廷, lab1:81, lab2:32, lab3:50, mid-term:90, final_exam: 93],
+	 * 	[ID:955002056, name:許文馨, lab1:88, lab2:92, lab3:88, mid-term:98, final_exam: 91],
+	 * 	[ID:962001044, name:凌宗廷, lab1:87, lab2:86, lab3:98, mid-term:88, final_exam: 87]}
+	 */
 	@Test
 	public void showGradeTestPass() {
 		GradeSystem TestGradeSys = new GradeSystem();
@@ -81,7 +100,15 @@ class StudentTests {
 		assertEquals("88 92 88 98 91", TestGradeSys.showGrade(studentList, position));
 		System.out.println("showGrade_1");
 	}
-	
+	/**
+	 * test function: GradeSystem.showGrade()
+	 * test data: 
+	 * ID:962001044
+	 * studentList: 
+	 * {[ID:962001051, name:李威廷, lab1:81, lab2:32, lab3:50, mid-term:90, final_exam: 93],
+	 * 	[ID:955002056, name:許文馨, lab1:88, lab2:92, lab3:88, mid-term:98, final_exam: 91],
+	 * 	[ID:962001044, name:凌宗廷, lab1:87, lab2:86, lab3:98, mid-term:88, final_exam: 87]}
+	 */
 	@Test
 	public void showGradeTestFail() {
 		GradeSystem TestGradeSys = new GradeSystem();
@@ -89,7 +116,15 @@ class StudentTests {
 		assertEquals("87 86 98 88 87", TestGradeSys.showGrade(studentList, position));
 		System.out.println("showGrade_2");
 	}
-	
+	/**
+	 * test function: GradeSystem.showValue()
+	 * test data: 
+	 * ID:962001044
+	 * studentList: 
+	 * {[ID:962001051, name:李威廷, lab1:81, lab2:32, lab3:50, mid-term:90, final_exam: 93],
+	 * 	[ID:955002056, name:許文馨, lab1:88, lab2:92, lab3:88, mid-term:98, final_exam: 91],
+	 * 	[ID:962001044, name:凌宗廷, lab1:87, lab2:86, lab3:98, mid-term:88, final_exam: 87]}
+	 */
 	@Test
 	public void showValueTestPass() {
 		GradeSystem TestGradeSys = new GradeSystem();
@@ -97,7 +132,15 @@ class StudentTests {
 		assertEquals("88.30", TestGradeSys.showValue(studentList, position));
 		System.out.println("showValue_1");
 	}
-	
+	/**
+	 * test function: GradeSystem.showValue()
+	 * test data: 
+	 * ID:955002056
+	 * studentList: 
+	 * {[ID:962001051, name:李威廷, lab1:81, lab2:32, lab3:50, mid-term:90, final_exam: 93],
+	 * 	[ID:955002056, name:許文馨, lab1:88, lab2:92, lab3:88, mid-term:98, final_exam: 91],
+	 * 	[ID:962001044, name:凌宗廷, lab1:87, lab2:86, lab3:98, mid-term:88, final_exam: 87]}
+	 */
 	@Test
 	public void showValueTestFail() {
 		GradeSystem TestGradeSys = new GradeSystem();
@@ -138,16 +181,22 @@ class StudentTests {
 		assertArrayEquals(test_weighted, TestGradeSys.changeWeight(weighted, studentList));
 		System.out.println("changeWeight_1");
 	}
-	
-
-	
+	/**
+	 * test function: GradeSystem.inputData()
+	 * test data: /test.txt
+	 * @throws IOException
+	 */
 	@Test
 	public void InputDataPass() throws IOException {
 		GradeSystem TestGradeSys = new GradeSystem();
 		assertEquals(1,TestGradeSys.InputData(studentList, weighted, "/test.txt")); 
 		System.out.println("InputData_1");
 	}
-	
+	/**
+	 * test function: GradeSystem.inputData()
+	 * test data: /wrong.txt
+	 * @throws IOException
+	 */
 	@Test
 	public void InputDataFail() throws IOException {
 		GradeSystem TestGradeSys = new GradeSystem();
@@ -157,7 +206,7 @@ class StudentTests {
 	
 
 	/**
-	 * test function: GradeSystem.showGrade()
+	 * test function: GradeSystem.calValue()
 	 * test data: 
 	 * ID:955002056
 	 * studentList: 
@@ -178,7 +227,16 @@ class StudentTests {
 		assertEquals("91.50", TestGradeSys.calValue(studentList, weighted, position));
 		System.out.println("calValue_1");
 	}
-	
+	/**
+	 * test function: GradeSystem.calValue()
+	 * test data: 
+	 * ID:955002056
+	 * studentList: 
+	 * {[ID:962001051, name:李威廷, lab1:81, lab2:32, lab3:50, mid-term:90, final_exam: 93],
+	 * 	[ID:955002056, name:許文馨, lab1:88, lab2:92, lab3:88, mid-term:98, final_exam: 91],
+	 * 	[ID:962001044, name:凌宗廷, lab1:87, lab2:86, lab3:98, mid-term:88, final_exam: 87]}
+	 * weight: 0.5, 0.1, 0.1, 0.1, 0.2
+	 */
 	@Test
 	public void calValueTestFail() {
 		GradeSystem TestGradeSys = new GradeSystem();
